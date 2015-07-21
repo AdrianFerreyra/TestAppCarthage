98bdf57139076e8aafec0896e4312003e4ba1773
Adrian Ferreyra <luis.ferrreyra@despegar.com>
2015-07-21 15:12:16 -0300

    first commit

---
+++ Carthage/Checkouts/CheckoutTest/TestTestFramework/TestTestFramework/TestTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestTestFramework.h
+//  TestTestFramework
+//
+//  Created by Adrian Ferreyra on 7/21/15.
+//  Copyright (c) 2015 Despegar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestTestFramework.
+FOUNDATION_EXPORT double TestTestFrameworkVersionNumber;
+
+//! Project version string for TestTestFramework.
+FOUNDATION_EXPORT const unsigned char TestTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestTestFramework/PublicHeader.h>
+
+
